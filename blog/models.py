79a847d339ea7feb0e7709000df0b715efea7d4a
Jakkrit Puts <jakkrit.puts@gmail.com>
2023-09-26 21:50:08 +0700

    edit: redesign

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from ckeditor.fields import RichTextField
 
 class Tag(models.Model):
-    tag_name_th = models.CharField(max_length=50)
-    tag_name_en = models.CharField(max_length=50)
+    tag_name = models.CharField(max_length=50)
     date = models.DateTimeField(auto_now_add=True, blank=False)
     date_updated = models.DateTimeField(auto_now=True, blank=False)
 
     def __str__(self):
-        return f'{self.tag_name_en}'
+        return f'{self.tag_name}'
 
 class Blog(models.Model):
     photo = models.ImageField(upload_to='Photo', default='')
@@ -18,6 +17,8 @@ class Blog(models.Model):
     publish = models.BooleanField(default=False, blank=False)
     date = models.DateTimeField(auto_now_add=True, blank=False)
     date_updated = models.DateTimeField(auto_now=True, blank=False)
+    short_detail = models.TextField(max_length=1000)
+    tag = models.ManyToManyField(Tag)
 
     def __str__(self):
         return f'{self.title}'
