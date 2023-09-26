5ae2c59c63abdcaa5c437566a997471221b973dc
Jakkrit Puts <jakkrit.puts@gmail.com>
2023-09-27 02:34:05 +0700

    add: breadcrumb

--- jstools/tailwind.config.js
+++ jstools/tailwind.config.js
@@ -1,20 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   future: {
       removeDeprecatedGapUtilities: true,
       purgeLayersByDefault: true,
   },
-  purge: {
-      enabled: false, //true for production build
-      content: [
+  enabled: false, //true for production build
+  content: [
           '../**/templates/*.html',
           '../**/templates/**/*.html',
           '../templates/*.html',
           '../templates/**/*.html'
-      ]
-  },
+      ],
   theme: {
-      extend: {},
+    fontFamily: {
+        display: ['Mitr', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        //just add this below and your all other tailwind colors willwork
+      ...colors
+ }
+    },
   },
   variants: {},
   plugins: [],
